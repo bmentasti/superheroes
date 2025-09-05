ef959fd6ee39419b2ece6348254e93a765f49586
Bruno Mentasti <bmentasti@hotmail.com>
2025-09-05 18:48:52 -0300

    update routes servers
    to deploy in vercel

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,9 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, type ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
+  { path: '', renderMode: RenderMode.Server },              
+  { path: 'heroes', renderMode: RenderMode.Server },
+  { path: 'heroes/new', renderMode: RenderMode.Server },
+  { path: 'heroes/:id/edit', renderMode: RenderMode.Server }, 
+  { path: '**', renderMode: RenderMode.Server },          
 ];
